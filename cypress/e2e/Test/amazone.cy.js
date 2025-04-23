6153812630f0388713077662ec974a9cd5c3488a
avinash <avipandey3792@gmail.com>
2025-04-23 21:54:28 +0530

    login chages

--- cypress/e2e/Test/amazone.cy.js
+++ cypress/e2e/Test/amazone.cy.js
@@ -4,7 +4,10 @@ describe(" TC1", () => {
      it(" tc2 ", ()=> {
 
        cy. visit("https://www.amazon.in/");
-        cy.contains("Today's Deals").click();
+        //cy.contains("Today's Deals").click(); 
+        cy.get("#nav-link-accountList-nav-line-1").click();
+        cy.get("#ap_email_login").eq(0).type("7683033803");
+        cy.get(".a-button-input").click();
     // })
    // cy. visit("https://www.amazon.in/");
     //cy.contains("Today's Deals").click();
